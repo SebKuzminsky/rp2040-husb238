ce2b44e4bf9e6d0b95f1605b8748896749f04df1
Sebastian Kuzminsky <seb@highlab.com>
2023-10-17 13:02:05 -0600

    cycle through PDOs/voltages in example program

--- example/main.cpp
+++ example/main.cpp
@@ -36,9 +36,44 @@ int main() {
         if (!husb238_connected(i2c)) {
             printf("HUSB238 not connected, check I2C wiring and USB-C connection.\n");
         } else {
+            printf("\x1b[2J");  // VT100: clear screen
             printf("HUSB238 connected!\n");
             husb238_dump_registers(i2c);
+
+            husb238_pdo_t pdos[6];
+            int r;
+
+            // Show available PDOs.
+            r =  husb238_get_pdos(i2c, pdos);
+            if (r != PICO_OK) {
+                printf("failed to read from husb238\n");
+                continue;
+            }
+            printf("available PDOs:\n");
+            for (int i = 0; i < 6; ++i) {
+                if (pdos[i].max_current > 0.0) {
+                    printf("%f V, %04.2fA\n", pdos[i].volts, pdos[i].max_current);
+                } else {
+                    printf("%f V (not available from this USB-PD source)\n", pdos[i].volts);
+                }
+            }
+
+            // Cycle through available PDOs.
+            for (int i = 0; i < 6; ++i) {
+                if (pdos[i].max_current > 0.0) {
+                    printf("\n");
+                    printf("selecting PDO (%f V, %04.2fA)\n", pdos[i].volts, pdos[i].max_current);
+                    r = husb238_select_pdo(i2c, pdos[i].id);
+                    if (r != PICO_OK) {
+                        printf("failed to select PDO: %d\n", r);
+                        break;
+                    }
+                    husb238_dump_registers(i2c);
+                    sleep_ms(3 * 1000);
+                }
+            }
         }
+
         sleep_ms(1000);
     }
 }
